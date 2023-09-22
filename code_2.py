d8a1e8bc4465272cdd80a9662af8f35baa96033f
segabaser <139810447+segabaser@users.noreply.github.com>
2023-09-22 03:02:43 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,25 +1,40 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-        }
-    }
-}
 
-        }
-        int[] data = new int[10];
-
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {def generate_random_data():
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+    return data
         print(f"Random Number: {item}")
-    main()
 
 import random
-def main():
+def generate_random_data():
+
+    data = generate_random_data()
 if __name__ == "__main__":
+
+    for item in data:
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()def generate_random_data():
+import random
+    for item in data:
+    data = generate_random_data()
 
 
     return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+        print(f"Random Number: {item}")
+
+def main():
+
+if __name__ == "__main__":if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+    return data
+import random
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    data = generate_random_data()
+    main()
     for item in data:
+def main():
